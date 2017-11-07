fb19971d48c151f09b10af140499472a6bcf62ab
wangjianbin <wangjianbin@onemt.com.cn>
2017-11-07 18:09:23 +0800

    init

---
+++ src/main/java/com/binge/datastucture/array/Array.java
@@ -0,0 +1,19 @@
+package com.binge.datastucture.array;
+
+/**
+ * @author binge
+ * @datetime 2017年11月7日
+ * @version
+ * @encoding UTF8
+ * @Description
+ */
+
+public class Array {
+
+    private long[] data;
+    private int size;
+
+    public Array(int max) {
+        a = new long[max];
+    }
+}
